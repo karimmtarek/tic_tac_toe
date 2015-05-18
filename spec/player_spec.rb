4d53a32b8a7c9aa4727f4de8b9ed5f81747f2eda
Karim Tarek <karimmtarek@gmail.com>
2015-05-18 02:53:05 -0400

    completed player specs

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,9 +3,67 @@ require 'spec_helper'
 module TicTacToe
   describe Player do
     context '#initialize' do
-      it 'creates empty players hash' do
+      it 'initiates an empty signs hash' do
         players = Player.new
-        expect(players.players).to eq({ human: '', computer: '' })
+        expect(players.signs).to eq({})
+      end
+
+      it 'initiates an empty moves array' do
+        players = Player.new
+        expect(players.moves).to eq([])
+      end
+    end
+
+    context '#toss' do
+      it 'assigns value to human and computer' do
+        players = Player.new
+        players.toss
+        expect(players.signs[:human].empty?).to be_falsey
+        expect(players.signs[:computer].empty?).to be_falsey
+      end
+    end
+
+    context '#print' do
+      it 'prints human vs. computer' do
+        players = Player.new
+        players.signs[:human] = 'X'
+        players.signs[:computer] = 'O'
+
+        expect(players.print).to eq('Human(X) vs. Computer(O)')
+      end
+
+      it 'prints computer vs. human' do
+        players = Player.new
+        players.signs[:computer] = 'X'
+        players.signs[:human] = 'O'
+
+        expect(players.print).to eq('Computer(X) vs. Human(O)')
+      end
+    end
+
+    context '#winner' do
+      it 'prints the winner' do
+        players = Player.new
+        players.moves << 'X'
+        players.signs[:human] = 'X'
+
+        expect(players.winner).to eq('Human')
+      end
+    end
+
+    context '#current' do
+      it 'returns current_palayer' do
+        players = Player.new
+
+        expect(players.current).to eq('X')
+      end
+    end
+
+    context '#switch' do
+      it 'switchs current player' do
+        players = Player.new
+
+        expect(players.switch).to eq('O')
       end
     end
   end
